0f216b837b0671655cd0a9bcca2f37f972d1665f
Vinicius Henrique <112169674+vinicius123henrique321@users.noreply.github.com>
2023-06-01 07:13:43 -0300

    update: connection factory done

--- ProvaDoisCrud/src/Factory/ConnectionFactory.java
+++ ProvaDoisCrud/src/Factory/ConnectionFactory.java
@@ -1,5 +1,16 @@
 package Factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    public Connection getConnection(){
+        try{
+            return DriverManager.getConnection("jdbc:mysql://localhost:/prova","root","vinielivia1@");
+        }
+        catch(SQLException excecao){
+            throw new RuntimeException(excecao);
+        }
+    }
 }
